671e309018de9df5b20f2f70283246a6231a5dd6
syedsubhan17 <120404440+syedsubhan17@users.noreply.github.com>
2023-12-01 04:53:35 +0000

    we have discussed dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,23 @@
 "use strict" // treat all JS code as newer version
 
 // alert("hello") //we are using nodeJS, not browser
+console.log(3 + 3)
+console.log("subhan")
+
+let name = "subhan"
+let age = 22
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+// boolean => true/false
+//null => standalone value
+//undefined => unSignedValue
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //objects
