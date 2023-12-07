71694ccf3c5f1545144623bf32aebfd70090f1c6
syedsubhan17 <120404440+syedsubhan17@users.noreply.github.com>
2023-12-07 19:15:59 +0000

    learnt more about objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,67 @@
+// const tinderUser = new Object() //singleton object
+const tinderUser = {}  // non singleton user
+
+tinderUser.id = "123abc"
+tinderUser.name = "subhan"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userFullName: {
+            firstname: "syed ",
+            lastname: "subhan",
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullName.firstname)
+// console.log(regularUser.fullname?.userFullName.firstname) //we talk about later
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = {obj1, obj2}
+// console.log(obj3)
+
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3);
+
+// const obj3 ={...obj1, ...obj2}
+// console.log(obj3)
+
+const users = [
+    {
+        id: 1,
+        email: "subhan@gmail.com"
+    },
+    {
+        id: 1,
+        email: "subhan@gmail.com"
+    },
+    {
+        id: 1,
+        email: "subhan@gmail.com"
+    },
+    {
+        id: 1,
+        email: "subhan@gmail.com"
+    },
+    {
+        id: 1,
+        email: "subhan@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')) // true
+console.log(tinderUser.hasOwnProperty('isLogged')) // false
