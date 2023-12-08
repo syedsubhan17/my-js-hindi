8129e2da13b8c99688536d9ca9168482cc4f4166
syedsubhan17 <120404440+syedsubhan17@users.noreply.github.com>
2023-12-08 17:34:13 +0000

    more about functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,34 @@ function loginUserMessage(username = "syed"){
 }
 
 // console.log(loginUserMessage("subhan"))
-console.log(loginUserMessage()) // ans will be undefined
+// console.log(loginUserMessage()) // ans will be undefined
+
+function calculateCartPrice(val1, val2, ...num1){//using the rest or spread operator
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "subhan",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "syed",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 10000]));
