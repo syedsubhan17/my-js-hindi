ebf5ebaa3d00f2024aff368366c2029e3465d2bc
syedsubhan17 <120404440+syedsubhan17@users.noreply.github.com>
2023-12-09 04:43:50 +0000

    learnt simled iffe and named iffe

--- 03_basics/04_iffe.js
+++ 03_basics/04_iffe.js
@@ -1,9 +1,13 @@
 // IIFE = Immediately Invoked Function Expression (IIFE)
 
 (function chai() {
-    console.log(`DB CONNECTED`);
+    console.log(`DB CONNECTED`); // this is named iffe
 })();
 
+( () => {
+    console.log(`DB CONNECTED TWO`);
+} )();
+
 ( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
+    console.log(`DB CONNECTED TWO ${name}`); // this is simple iffe
 } )(`subhan`)
