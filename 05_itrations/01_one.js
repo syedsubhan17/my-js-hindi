084ce9792d425a97da45ceaf26ce56bbeec16540
syedsubhan17 <120404440+syedsubhan17@users.noreply.github.com>
2023-12-11 13:41:19 +0000

    learnt for loop

---
+++ 05_itrations/01_one.js
@@ -0,0 +1,50 @@
+// for
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+//     if (element == 5) {
+//         console.log("5 is the best");    
+//     }
+//     //console.log(element);
+// }
+
+//console.log(element)
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value: ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         // console.log(`Inner loop value ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j + ' = ' + i*j );               
+//     }
+    
+// }
+
+let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element)   
+}
+
+//breake and continue
+
+//break
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break;
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
+//continue
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
