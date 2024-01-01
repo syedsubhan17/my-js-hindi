98a5e21e8b2149d47682c18e0180d9d91ae318a6
kulsum_syed_18 <syedkulsum18@gmail.com>
2024-01-01 13:09:42 +0530

    learnt what is inheritance in JS

---
+++ 10_Classes_And_OOP/inheritance.js
@@ -0,0 +1,35 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`a new cousre was added by ${this.username}`)
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "1234")
+
+chai.addCourse()
+chai.logMe()
+
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai === masalaChai);
+console.log(chai === Teacher);
+console.log(chai instanceof Teacher);
+console.log(chai instanceof User);
