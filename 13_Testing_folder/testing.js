015f56c092960fbd4eb58a8f4ce64d9b84e5aa61
syedsubhan17 <120404440+syedsubhan17@users.noreply.github.com>
2024-01-17 07:58:49 +0000

    testing no 2

--- 13_Testing_folder/testing.js
+++ 13_Testing_folder/testing.js
@@ -1,4 +1,8 @@
-let books = ["Html", "Css", "Javascript"];
-console.log(books[0]); // Html
-console.log(books[1]); // Css
-console.log(books[2]); // Javascript
+// mix of values
+let arr = ['Javascript', { sitename: 'W3Docs'}, true, function () {
+    console.log('welcome');
+  }];
+  // get the object at index 1 and then show its name
+  console.log(arr[1].sitename); // W3Docs
+  // get the function at index 3 and run it
+  arr[3](); // welcome
