d81914a90735502c9a8d5ea46744a49bafad83dd
syedsubhan17 <120404440+syedsubhan17@users.noreply.github.com>
2024-05-22 04:30:35 +0000

    Types of data types

---
+++ NewJSlearning/01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+console.log("SUBHAN");
+
+let name = "subhan"
+let age = 18
+let isLoggedIn = false
+
+//  number => to power 53
+//  bigint
+//  string => " "
+//  boolean => true/false
+//  null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Subhan");  // =>string
+console.log(typeof 32); // =>number
+console.log(typeof null); // =>object
+console.log(typeof undifine); // => undefine
